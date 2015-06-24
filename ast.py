1c219f3ef9f6f094d190c322063683da862af607
Eric Love <ericlove@eecs.berkeley.edu>
2015-06-24 14:19:28 -0700

    Stringification updates

--- ast.py
+++ ast.py
@@ -35,6 +35,9 @@ class Expr:
     """PyRRHIC Expression AST"""
     lineInfo = None
     
+    # Kinds of expressinos that are available
+    BinExprType, UnExprType, LitType = range(3)
+    
     def __add__(self, other):
         return Add(self, other)
     
@@ -44,20 +47,37 @@ class Expr:
     def __invert__(self):
         return Invert(self)
     
+    def parens(self):
+        """
+        Adds parentheses around the string representation of this expression
+        if it is not a literal.  Used in larger composit expressions' string
+        representations.
+        """
+        if self.exprType == Expr.LitType:
+            return str(self)
+        else:
+            return "("+str(self)+")"
+    
 class Lit(Expr):
     """PyRRHIC Literal Expression"""
+    
+    exprType = Expr.LitType
+    
     def __init__(self, value):
         self.value = value
         self.lineInfo = LineInfo(2)
+        
     def __str__(self):
         return str(self.value)
 
 class BinExpr(Expr):
+    exprType = Expr.BinExprType
+    
     def __init__(self, a, b):
         self.a = a
         self.b = b
     def __str__(self):
-        return str(self.a) + " " + self.symbol + " " + str(self.b)
+        return self.a.parens() + " " + self.symbol + " " + self.b.parens()
 
 class Add(BinExpr):
     symbol = "+"
@@ -70,10 +90,12 @@ class Sub(BinExpr):
         BinExpr.__init__(self, a, b)
 
 class UnExpr(Expr):
+    exprType = Expr.UnExprType
+    
     def __init__(self, e):
         self.e = e
     def __str__(self):
-        return self.symbol + str(self.e)
+        return self.symbol + self.e.parens()
 
 class Invert(UnExpr):
     symbol = "~"
