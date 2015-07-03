8650446019aefbe410cebce56054ff2a5beebd64
Eric Love <ericlove@eecs.berkeley.edu>
2015-07-03 09:58:15 -0700

    derive Expr from object

--- pyrast/expr.py
+++ pyrast/expr.py
@@ -1,30 +1,30 @@
 
 
-class Expr:
+class Expr(object):
     """PyRRHIC Expression AST"""
     lineInfo = None
-    
+
     # Set to true if no parentheses are needed around this expression
     isSingleTerm = False
-    
+
     def __add__(self, other):
         return Add(self, other)
-    
+
     def __sub__(self, other):
         return Sub(self, other)
-    
+
     def __invert__(self):
         return Invert(self)
-        
+
     def __getslice__(self, a, b):
         return Bits(self, a, b)
-    
+
     def __gt__(self, other):
         return Gt(self, other)
-    
+
     def __lt__(self, other):
         return Lt(self, other)
-    
+
     def parens(self):
         """
         Adds parentheses around the string representation of this expression
@@ -35,13 +35,13 @@ class Expr:
             return str(self)
         else:
             return "("+str(self)+")"
-    
+
     def traverse(self, func):
         """
         Performs a pre-order traversal of this `Expr` AST and applies `func`
         to each node encountered along the way, replacing it with the
         result returned by `func`.
-        
+
         Note that this function modifies the AST on which it operates.
         """
         return func(self)
@@ -49,22 +49,22 @@ class Expr:
 class Lit(Expr):
     """PyRRHIC Literal Expression"""
     isSingleTerm = True
-    
+
     def __init__(self, value, width = None, signed = False):
         self.value = value
         self.width = width
         self.signed = signed
-        
+
     def __str__(self):
         return str(self.value)
 
 
 class Id(Expr):
     isSingleTerm = True
-    
+
     def __init__(self, idt):
         self.idt = idt
-        
+
     def __str__(self):
         return str(self.idt)
     def __repr__(self):
@@ -74,10 +74,10 @@ class BinExpr(Expr):
     def __init__(self, a, b):
         self.a = a
         self.b = b
-    
+
     def __str__(self):
         return self.a.parens() + " " + self.symbol + " " + self.b.parens()
-    
+
     def traverse(self, func):
         self.a = self.a.traverse(func)
         self.b = self.b.traverse(func)
@@ -107,20 +107,20 @@ class Invert(UnExpr):
     symbol = "~"
     def __init__(self, e):
         UnExpr.__init__(self, e)
-  
+
 class SReg(Expr):
     def __init__(self, value, enable = None):
         self.value = value
         self.enable = enable
     def __str__(self):
         if self.enable != None:
-            return "Reg("+str(self.value)+", "+str(self.enable)+")"     
+            return "Reg("+str(self.value)+", "+str(self.enable)+")"
         else:
             return "Reg("+str(self.value)+")"
     def traverse(self, func):
         self.value = self.value.traverse(func)
         return func(self)
- 
+
 class Bits(Expr):
     def __init__(self, e, msb, lsb):
         self.e = e
@@ -156,24 +156,24 @@ class Neq(BinExpr):
     symbol = "!="
     def __init__(self, a, b):
         BinExpr.__init__(self, a, b)
-        
+
 class Lt(BinExpr):
     symbol = "<"
     def __init__(self, a, b):
         BinExpr.__init__(self, a, b)
-        
+
 class Gt(BinExpr):
     symbol = ">"
     def __init__(self, a, b):
         BinExpr.__init__(self, a, b)
-        
+
 class Mux(Expr):
     def __init__(self, sel, a, b):
         self.a = a
         self.b = b
         self.sel = sel
     def __str__(self):
-        return "Mux(" + str(self.sel) + ", " + str(self.a) + ", " + str(self.b) + ")"        
+        return "Mux(" + str(self.sel) + ", " + str(self.a) + ", " + str(self.b) + ")"
     def traverse(self, func):
         self.sel = self.sel.traverse(func)
         self.a = self.a.traverse(func)
