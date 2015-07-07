45443112a068161b0fa93f5af9daaa410fa7ce92
Eric Love <ericlove@eecs.berkeley.edu>
2015-07-07 16:47:41 -0700

    new tester

--- pyrrhic.py
+++ pyrrhic.py
@@ -12,3 +12,11 @@ if len(sys.argv) < 2:
 for a in sys.argv[1:]:
   code = astgen.compile_pyrrhic(a)
   exec(code)
+
+builder.elaborate_all_instances()
+
+print "\n\n---------------\n"
+for ctx in builder.elaboratedInstances:
+    mdec = builder.elaboratedInstances[ctx]
+    print str(mdec)
+    print "\n"
--- test.py
+++ test.py
@@ -2,46 +2,42 @@ from pyrast import *
 from builder.BuilderAST import *
 
 class ReadyValIO(BundleDec):
-    ready = UInt(1)
-    valid = Reverse(UInt(1))
-
-class RVData(ReadyValIO):
+    ready = Reverse(UInt(1))
+    valid = UInt(1)
     def __init__(self, data):
-        self.data = data
+      self.data = data
 
-class FIFO32IO(BundleDec):
-    input = RVData(UInt(32))
-    output = RVData(UInt(32))
+class FIFOIO(BundleDec):
+    def __init__(self, data):
+      self.input = ReadyValIO(data)
+      self.output = Reverse(ReadyValIO(data))
 
 class FIFO32(Module):
-
     def __init__(self, stages):
-        self.io = Wire(FIFO32IO())
-        r = Wire(RVData(UInt(32)))
+        self.io = Wire(FIFOIO(UInt(32)))
+        r = Wire(FIFOIO(UInt(32)))
+        Connect(r, self.io.input)
         for n in range(stages):
-            rnext = Reg(RVData(UInt(32)))
+            rnext = Reg(ReadyValIO(UInt(32)))
             Connect(rnext, r)
             r = rnext
-        #Connect(self.io.output, r)
+        Connect(self.io.output, r)
 
 class OtherModule(Module):
-    io = ReadyValIO()
-    r = Reg(type=UInt(1), onReset=Lit(0))
+    io = UInt(1)
 
-    def make_reg(width=1):
-        mr = Reg(type=UInt(width))
-        w = Wire(UInt(width))
-        print "W = " + str(w)
-        Connect(w, mr)
-        tm = Module(FIFO32(6))
-        Connect(tm.io.input.valid, w)
+    def make_fifo(stages, input):
+        tm = Module(FIFO32(stages))
+        Connect(tm.io.input, input)
+        w = Wire(FIFOIO(UInt(32)))
+        Connect(w, tm.io.output)
         return w
 
-    r1 = make_reg(width=2)
-    r2 = make_reg(width=3)
+    r1 = make_fifo(stages=2, input=Lit(0))
+    r2 = make_fifo(stages=5, input=Lit(1))
 
     #Connect(r, io.valid)
     def __init__(self):
-        print "INiting OtherModule"
+        print "Initing OtherModule"
 
 m = Module(OtherModule())
